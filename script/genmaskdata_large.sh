98619ba9f8788331ae19d798f8305f8037ed8a1b
ruanhuabin <ruanhuabin@foxmail.com>
2016-08-18 11:20:17 +0800

    add new topN algorithm that base on quicksort in erroranalysis.c

---
+++ script/genmaskdata_large.sh
@@ -0,0 +1,24 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE:  genmaskdata.sh
+# 
+#         USAGE:  ./genmaskdata.sh 
+# 
+#   DESCRIPTION:  
+# 
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR: YOUR NAME (), 
+#       COMPANY: 
+#       CREATED: 2016年08月14日 22时36分17秒 CST
+#      REVISION:  ---
+#===============================================================================
+
+for((i=0; i<=32; i++))
+do
+    ./erasebytes_c -i ../data/stack.mrc -o mask${i}bits.mrc -b $i
+done
+
